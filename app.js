da13a4ef356034ed93b43db800ec9d53002131a4
raseacyan <caesar821009@gmail.com>
2020-09-18 10:35:09 +0630

    test

--- app.js
+++ app.js
@@ -123,8 +123,6 @@ bot.onSubscribe(response => {
     say(response, `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me if a web site is down for everyone or just you. Just send me a name of a website and I'll do the rest!`);
 });
 
-
-const message = new TextMessage("Welcome to my tea shop",KEYBOARD_JSON,null,null,null,3);
 let KEYBOARD_JSON = {
         "Type": "keyboard",
         "DefaultHeight": true,
@@ -135,6 +133,9 @@ let KEYBOARD_JSON = {
             "TextSize": "regular"
         }]
     };
+    
+const message = new TextMessage("Welcome to my tea shop",KEYBOARD_JSON,null,null,null,3);
+
 
 bot.onConversationStarted((userProfile, isSubscribed, context) =>
     bot.sendMessage(userProfile,message)
