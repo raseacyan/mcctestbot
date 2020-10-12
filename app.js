d9cf99a38d871ae387fa2d5067941c7ebf99cb58
raseacyan <caesar821009@gmail.com>
2020-10-12 19:26:13 +0630

    added new page

--- app.js
+++ app.js
@@ -84,13 +84,15 @@ app.get('/',function(req,res){
     res.send('your app is up and running');
 });
 
-app.get('/addstock',function(req,res){    
-     res.render('addstock2.ejs');
+app.get('/addstock/:fruit',function(req,res){ 
+     let fruit = req.params.fruit;   // $_GET['fruit']
+
+     res.render('addstock2.ejs', {F: fruit});
 });
 
 app.post('/addstock',function(req,res){    
-     let name = req.body.name; 
-     let email = req.body.email
+     let name = req.body.name;  // $_POST['name']
+     let email = req.body.email // $_POST['email']
     // res.send('your name is: ' + name + " and your email is " + email);
     res.json({name:name, email:email});
 });
